3943818f3f31363e8c6342d13182d1ac6ea291f0
Ruslan-Vladyslav <rgritsai95@gmail.com>
2025-10-08 23:49:25 +0300

    Modify config file for deployment

--- Lab3/config.py
+++ Lab3/config.py
@@ -3,10 +3,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-POSTGRES_USER = os.getenv('POSTGRES_USER')
-POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
-POSTGRES_DB = os.getenv('POSTGRES_DB')
-DB_HOST = os.getenv('DB_HOST')
+if os.getenv("DATABASE_URL"):
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+else:
+    POSTGRES_USER = os.getenv('POSTGRES_USER')
+    POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+    POSTGRES_DB = os.getenv('POSTGRES_DB')
+    DB_HOST = os.getenv('DB_HOST', 'localhost')
 
-SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{DB_HOST}:5432/{POSTGRES_DB}"
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{DB_HOST}:5432/{POSTGRES_DB}"
+    )
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
