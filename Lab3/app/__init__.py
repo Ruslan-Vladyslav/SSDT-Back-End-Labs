392f212caa6cd04032de71641b7b28d8033b0be3
Ruslan-Vladyslav <rgritsai95@gmail.com>
2025-10-08 13:24:48 +0300

    Add congig file

--- Lab3/app/__init__.py
+++ Lab3/app/__init__.py
@@ -1,16 +1,22 @@
 from flask import Flask
-from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-import os
+from flask_migrate import Migrate
 
-app = Flask(__name__)
+db = SQLAlchemy()
+migrate = Migrate()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
-    'DATABASE_URL', 'sqlite:///local.db'
-)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+def create_app():
+    app = Flask(__name__)
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+    app.config.from_pyfile('config.py', silent=True)
 
-from app.views import users, categories, records, healthcheck
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.views import users, categories, records, healthcheck
+    app.register_blueprint(users.bp)
+    app.register_blueprint(categories.bp)
+    app.register_blueprint(records.bp)
+    app.register_blueprint(healthcheck.bp)
+
+    return app
--- Lab3/test_db.py
+++
@@ -1,9 +0,0 @@
-from app import app, db
-from sqlalchemy import text
-
-with app.app_context():
-    try:
-        result = db.session.execute(text("SELECT 1"))
-        print("Database is working!")
-    except Exception as e:
-        print("Database connection failed:", e)
