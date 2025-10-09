2a7386456762be370885c1e06c6a60b8733e2f7d
Ruslan-Vladyslav <rgritsai95@gmail.com>
2025-10-09 11:12:05 +0300

    Preparing project for lab 4

---
+++ Lab4/app/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+import os
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+jwt = JWTManager() 
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('../config.py', silent=False)
+
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)  
+
+    from app.views import user_bp, category_bp, record_bp, healthcheck_bp
+    app.register_blueprint(user_bp)
+    app.register_blueprint(category_bp)
+    app.register_blueprint(record_bp)
+    app.register_blueprint(healthcheck_bp)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
